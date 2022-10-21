aede7a008ea947dcf2b4694d0f8950a4b1e30c17
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-10-21 10:43:20 +1000

    Задание 5

--- Page12.xaml.cs
+++ Page12.xaml.cs
@@ -20,9 +20,34 @@ namespace YP5_PagePrimer_Cherednichenko
     /// </summary>
     public partial class Page12 : Page
     {
+
+
         public Page12()
         {
             InitializeComponent();
+
+            List<Stroka> strokas = new List<Stroka>();
+            strokas.Add(new Stroka() { Id = 1, Mesto = "Вход в офис", Toch="У двери", SNom = "55404403", IP = "192.163.201.89", MAC = "00:60:38:22:02:CE", SostPod = "Подключен" , Det = new DateTime(2016, 6, 24)});
+
+            dg1.ItemsSource = strokas;
+
+            
+
         }
+
+        public class Stroka
+        {
+            public int Id { get; set; }
+
+            public string Mesto { get; set; }
+            public string Toch { get; set; }
+            public string SNom { get; set; }
+            public string IP { get; set; }
+            public string MAC { get; set; }
+            public string SostPod { get; set; }
+            public DateTime Det { get; set; }
+
+        }
+
     }
 }
